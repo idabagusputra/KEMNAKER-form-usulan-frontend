391c15eb7f535310adaf1cb7169775ce410d6e23
salman261002 <116475964+MuhammadSalmanAlfarisi@users.noreply.github.com>
2023-02-01 16:11:42 +0700

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='w-full bg-gray-200 bottom-0 left-0 h-24'>Contact:</div>
+  )
+}
+
+export default Footer
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='w-full bg-gray-100 top-0 left-0 h-24 flex flex-row'>
+        <img src=".\src\assets\logo.svg" alt="Logo Kemnaker" className='w-16 m-4'/>
+    </div>
+  )
+}
+
+export default Navbar
